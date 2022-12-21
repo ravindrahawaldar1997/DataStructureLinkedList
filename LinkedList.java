534ce86c0d3c2dbb85c59e12ac38213f00f77617
ravindrahawaldar1997 <hawaldarravindra@gmail.com>
2022-12-21 19:09:10 +0530

    Added: program for creating simple linked list

---
+++ LinkedList.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.linkedlist;
+public class LinkedList {
+    public static void main(String[] args) {
+        Node firstNode=new Node(56);
+        Node secondNode=new Node(30);
+        Node thirdNode=new Node(70);
+
+        Node head=firstNode;
+        firstNode.next=secondNode;
+        secondNode.next=thirdNode;
+        Node tail=thirdNode;
+
+        Node temp =head;
+        while(temp!=null) {
+            System.out.println(temp.key);
+            temp= temp.next;
+        }
+    }
+}
---
+++ Node.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.linkedlist;
+public class Node {
+    int key;
+    Node next;
+    public Node(int key) {
+        this.key = key;
+        this.next = null;
+    }
+}
